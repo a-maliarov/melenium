84233424e7e62b93edc4e9d9aa20ae957e22a0de
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2020-11-01 08:34:34 +0200

    Move WaitFor into separated file

--- melenium/webdriver.py
+++ melenium/webdriver.py
@@ -139,33 +139,6 @@ class ChromeCapabilities(object):
 
 #-----------------------------------------------------------------------------
 
-class WaitFor(object):
-
-    def __init__(self, driver):
-        self._driver = driver
-
-    def phrase_in_link(self, timeout, phrase='/'):
-        counter = 0
-
-        while phrase not in self._driver.current_url:
-            time.sleep(1)
-            counter += 1
-
-            if counter == timeout:
-                return None
-
-    def element_in_dom(self, timeout, *argv, **kwargs):
-        counter = 0
-
-        while BS(self._driver.page_source, features = 'html.parser').find(*argv, **kwargs) == None:
-            time.sleep(1)
-            counter += 1
-
-            if counter == timeout:
-                return None
-
-#-----------------------------------------------------------------------------
-
 class ChromeDriver(Chrome):
 
     def __init__(self, executable_path="chromedriver", port=0,
