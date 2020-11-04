b0acec4a95f74f3e16a5a48f407c70e20bd65075
Anatolii <53064218+a-maliarov@users.noreply.github.com>
2020-11-04 09:33:22 +0200

    Update version to 0.0.4

--- melenium/__version__.py
+++ melenium/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'melenium'
 __description__ = 'Routine automation.'
 __url__ = 'https://github.com/a-maliarov/melenium'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Anatolii Maliarov'
 __author_email__ = 'tly.mov@gmail.com'
 __license__ = 'MIT'
